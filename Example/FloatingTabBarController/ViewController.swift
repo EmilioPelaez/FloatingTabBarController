48d17ad7f5f891b3aa299c3426aa958e63ab3766
Emilio Peláez <me@epelaez.net>
2019-04-28 11:29:43 -0500

    Added example

--- Example/FloatingTabBarController/ViewController.swift
+++ Example/FloatingTabBarController/ViewController.swift
@@ -2,23 +2,30 @@
 //  ViewController.swift
 //  FloatingTabBarController
 //
-//  Created by EmilioPelaez on 04/28/2019.
+//  Created by EmilioPelaez on 04/25/2019.
 //  Copyright (c) 2019 EmilioPelaez. All rights reserved.
 //
 
 import UIKit
+import SlidingTabBarController
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+class ViewController: FloatingTabBarController {
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		tabBar.tintColor = UIColor(white: 0.75, alpha: 1)
+		
+		viewControllers = (1...3).map { "Tab\($0)" }.map {
+			let selected = UIImage(named: $0 + "_Large")!
+			let deselected = UIImage(named: $0 + "_Small")!
+			let controller = storyboard!.instantiateViewController(withIdentifier: $0)
+			controller.title = $0
+			controller.view.backgroundColor = UIColor(named: $0)
+			controller.floatingTabItem = FloatingTabItem(selectedImage: selected, deselectedImage: deselected)
+			return controller
+		}
+		
+	}
+	
 }
-
